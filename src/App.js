1605ba7117fef91024e24d4f2b67b92cb7a1d151
OhSva <somkamolv@hotmail.com>
2020-10-11 01:54:52 +0700

    using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from 'react';
 
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build an Amazon clone app with React JS</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <h1>HOME PAGE!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
