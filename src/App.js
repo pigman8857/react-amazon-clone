a146d39f418860a623d742e8562483b64b36331e
OhSva <somkamolv@hotmail.com>
2020-10-17 15:35:05 +0700

    banner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
 import './App.css';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './Header';
+import Home from './Home';
+
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -17,7 +18,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>HOME PAGE!</h1>
+            <Home></Home>
           </Route>
         </Switch>
       </div>
